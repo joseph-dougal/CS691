2a82e96b061642e41df66d602f263372a8aa39f7
Yucked <yucked@protonmail.com>
2021-10-13 22:42:57 -0400

    use SERVER_NAME to configure host and port

--- project/config.py
+++ project/config.py
@@ -4,6 +4,7 @@
 
 DEBUG = True
 FLASK_ENV = 'development'
+SERVER_NAME = "localhost:5001"
 SECRET_KEY = 'JD_12345'
 SQLALCHEMY_DATABASE_URI = "sqlite:///sqlite-db.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
