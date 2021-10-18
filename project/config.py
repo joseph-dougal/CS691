3b1ccdcdc7b5e21091b65e950000c5e2c3f5a242
Joseph Dougal <jkdougal@gmail.com>
2021-10-17 20:32:08 -0400

    removing server setting from config - messing up prod

--- project/config.py
+++ project/config.py
@@ -4,7 +4,6 @@
 
 DEBUG = True
 FLASK_ENV = 'development'
-SERVER_NAME = "localhost:5001"
 SECRET_KEY = 'JD_12345'
 SQLALCHEMY_DATABASE_URI = "sqlite:///sqlite-db.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
